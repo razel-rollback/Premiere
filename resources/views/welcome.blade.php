f74034ee5b86b078a0200d57f995779ee4e3b367
Your Name <your-email@example.com>
2025-04-04 20:34:00 +0800

    Added welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,15 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+@section('title', 'Welcome')
 
-<body>
-    tour nin
-
-</body>
-
-</html>
+@section('content')
+<div class="text-center">
+    <h1>Welcome to Student Admission System</h1>
+    <p>This is the homepage.</p>
+    <a href="#" class="btn btn-primary">Get Started</a>
+</div>
+@endsection
