6ae2b013acf754198f1221839a3dec6322f72446
Your Name <your-email@example.com>
2025-04-14 00:39:58 +0800

    model 1.1

--- database/migrations/2025_04_12_113609_create_student_subjects_table.php
+++ database/migrations/2025_04_12_113609_create_student_subjects_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('student_subjects', function (Blueprint $table) {
-            $table->id();
+            $table->id('studentSubjectID');
+            $table->unsignedBigInteger('studentID');
+            $table->unsignedBigInteger('subjectID');
             $table->timestamps();
         });
     }
